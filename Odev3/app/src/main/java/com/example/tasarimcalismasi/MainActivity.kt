1b9bf55cdb205dcc7accfca58540763f6016792d
Büşra Altuntaş <99415208+busraaltuuntas@users.noreply.github.com>
2023-04-03 01:35:04 +0300

    o

---
+++ Odev3/app/src/main/java/com/example/tasarimcalismasi/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.tasarimcalismasi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //R = ress klasörü(kodlama içinde geçerli)
+        //@= res klasörü( Xml dosyası içerisinde geçerli)
+    }
+}
